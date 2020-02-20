433d46e7ca28ed931654b87dc860f76742b85ed2
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-02-20 09:42:00 +0100

    Added new function

--- LEDs/PWM-BINARY/ledsPWM.cpp
+++ LEDs/PWM-BINARY/ledsPWM.cpp
@@ -1,10 +1,12 @@
 #include "mbed.h"
 
 // Constant variables
-static const int g_numOfPwmLeds = 10;
+static const int g_numOfPwmLeds = 14;
 static const float ledIncrease = 0.1;
 static const int g_numOfRedLeds = 10;
 static const int g_numOfButtons = 4;
+// Global variables
+int currentIndex = 0; //for colourpicker function
 
 // Serial line for printf output
 Serial g_pc(USBTX, USBRX);
@@ -16,11 +18,20 @@ DigitalOut g_redLeds[g_numOfRedLeds] = {(PTA1),(PTA1),(PTC0),(PTC1),(PTC2),(PTC3
 DigitalIn g_buttons[g_numOfButtons] = {(PTC9),(PTC10),(PTC11),(PTC12)};
 
 // PwmLed struct definition
-struct PwmLed{
-		DigitalOut led;
+class PwmLed{
+	private:
+		DigitalOut *led; //Pointer to led object
 		float brightness;
-		static const int timeUnit = 10; //Number of all states
-		int timeFrame;
+		static const int timeUnit = 15; //Number of all states
+		int timeFrame; //One state
+
+	public:
+		PwmLed(PinName pin, float brightness)
+		{
+			this->led = new DigitalOut(pin);
+			this->brightness = brightness;
+			this->timeFrame = 0;
+		}
 
 		//Just a setter
 		void setBrightness(float brightness){
@@ -28,48 +39,107 @@ struct PwmLed{
 		}
 		void update() //timeFrame is one state of all states in timeUnit
 		{
-			if(timeFrame == 15){
-				timeFrame = 0;
+			if(this->timeFrame == timeUnit){
+				this->timeFrame = 0;
 			}
-			if (timeFrame < (timeUnit * brightness))
+			if (this->timeFrame < (this->timeUnit * this->brightness))
 			{
-				led = true;
+				this->led->write(1);
 			}
 			else
 			{
-				led= false;
+				this->led->write(0);
 			}
-			timeFrame++;
+			this->timeFrame++;
 		}
 
 		//Just a getter
 		int getTimeUnit(){
-			return timeUnit;
+			return this->timeUnit;
 		}
 		//Just a getter
 		float getBrightness(){
-			return brightness;
+			return this->brightness;
 		}
 };
 
-// Top leds on K64F-KIT -> creation of PwmStructs...
+// Top leds on K64F-KIT -> creation of Pwm objects...
 PwmLed pwmLeds[g_numOfPwmLeds] =
 {
-	{ PTC0, 0, 0},
-	{ PTC1, 0, 0 },
-	{ PTC2, 0, 0 },
-	{ PTC3, 0, 0 },
-	{ PTC4, 0, 0 },
-	{ PTC5, 0, 0 },
-	{ PTC7, 0, 0 },
-	{ PTC8, 0, 0 },
-	{ PTB9, 0, 0},
-	{ PTB19, 0, 0}
+	{ PTC0, 0},
+	{ PTC1, 0},
+	{ PTC2, 0},
+	{ PTC3, 0},
+	{ PTC4, 0},
+	{ PTC5, 0},
+	{ PTC7, 0},
+	{ PTC8, 0},
+	{ PTB2, 0}, //B 8
+	{ PTB3, 0}, //G 9
+	{ PTB9, 0}, //R 10
+	{ PTB11, 0}, //B 11
+	{ PTB18, 0}, //G 12
+	{ PTB19, 0} //R 13
+
 };
 
+//Calling update function on all leds in array
 void updater(){
 	for (int l_currentLed = 0; l_currentLed < g_numOfPwmLeds; l_currentLed++){
 		pwmLeds[l_currentLed].update();
+	}
+}
+
+//For fun :D
+void policie(){;
+	pwmLeds[8].setBrightness(1.0);
+	pwmLeds[10].setBrightness(0);
+	pwmLeds[11].setBrightness(0);
+	pwmLeds[13].setBrightness(1.0);
+	wait_ms(100);
+	pwmLeds[8].setBrightness(0);
+	pwmLeds[10].setBrightness(1.0);
+	pwmLeds[11].setBrightness(1.0);
+	pwmLeds[13].setBrightness(0);
+
+}
+
+
+void colourPicker(){
+
+	if(!g_buttons[3]){
+		if(currentIndex == g_numOfPwmLeds - 1){
+			currentIndex = 0;
+		}
+		else{
+			currentIndex++;
+		}
+		g_pc.printf("Current INDEX: ");
+		g_pc.printf("%d\r\n", currentIndex);
+		wait_ms(150);
+	}
+	if(!g_buttons[2]){
+		if(currentIndex == 0){
+			currentIndex = g_numOfPwmLeds - 1;
+		}
+		else{
+			currentIndex--;
+		}
+		g_pc.printf("Current INDEX: ");
+		g_pc.printf("%d\r\n", currentIndex);
+		wait_ms(150);
+	}
+	if(!g_buttons[1] && pwmLeds[currentIndex].getBrightness() < 1.0){
+		pwmLeds[currentIndex].setBrightness(pwmLeds[currentIndex].getBrightness()+ledIncrease);
+		g_pc.printf("Current BRIGHTNESS: ");
+		g_pc.printf("%f\r\n", pwmLeds[currentIndex].getBrightness()*10);
+		wait_ms(100);
+	}
+	if(!g_buttons[0] && pwmLeds[currentIndex].getBrightness() > 0.0){
+		pwmLeds[currentIndex].setBrightness(pwmLeds[currentIndex].getBrightness()-ledIncrease);
+		g_pc.printf("Current BRIGHTNESS: ");
+		g_pc.printf("%f\r\n", pwmLeds[currentIndex].getBrightness()*10);
+		wait_ms(100);
 	}
 }
 
@@ -77,49 +147,16 @@ int main()
 {
 	// Serial line initialization
  	g_pc.baud(115200);
-	int currentIndex = 0;
 
-	// Increasing led brightness from 0% to 100% by 10% repetitively
+
 	Ticker ticker;
 	ticker.attach_us(callback(updater),1000);
 
+
 	while (1)
 	{
-		if(!g_buttons[3]){
-			if(currentIndex == g_numOfPwmLeds - 1){
-				currentIndex = 0;
-			}
-			else{
-				currentIndex++;
-			}
-			g_pc.printf("Current INDEX: ");
-			g_pc.printf("%d\r\n", currentIndex);
-			wait_ms(150);
-		}
-		if(!g_buttons[2]){
-			if(currentIndex == 0){
-				currentIndex = g_numOfPwmLeds - 1;
-			}
-			else{
-				currentIndex--;
-			}
-			g_pc.printf("Current INDEX: ");
-			g_pc.printf("%d\r\n", currentIndex);
-			wait_ms(150);
-		}
-		if(!g_buttons[1] && pwmLeds[currentIndex].getBrightness() < 1.0){
-			pwmLeds[currentIndex].setBrightness(pwmLeds[currentIndex].getBrightness()+ledIncrease);
-			g_pc.printf("Current BRIGHTNESS: ");
-			g_pc.printf("%f\r\n", pwmLeds[currentIndex].getBrightness()*10);
-			wait_ms(100);
-		}
-		if(!g_buttons[0] && pwmLeds[currentIndex].getBrightness() > 0.0){
-			pwmLeds[currentIndex].setBrightness(pwmLeds[currentIndex].getBrightness()-ledIncrease);
-			g_pc.printf("Current BRIGHTNESS: ");
-			g_pc.printf("%f\r\n", pwmLeds[currentIndex].getBrightness()*10);
-			wait_ms(100);
-		}
+		//policie();
+		//colourPicker();
 	}
 }
-
 
