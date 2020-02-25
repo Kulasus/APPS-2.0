ab1c125e1341f68c80da2c8a76f5191ce22ea9a8
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-02-25 13:38:16 +0100

    Added signal function, fixed colourPicker

--- LEDs/PWM-BINARY/ledsPWM.cpp
+++ LEDs/PWM-BINARY/ledsPWM.cpp
@@ -1,22 +1,25 @@
+
 #include "mbed.h"
 
 // Constant variables
 static const int g_numOfPwmLeds = 14; // All top redLeds + All rgbLeds = 14
-static const float ledIncrease = 0.1; // 10% Increase of led brightness
+static const float ledIncrease = 0.05; // 10% Increase of led brightness
 static const int g_numOfRedLeds = 10; // Both top and bottom redLeds
 static const int g_numOfRgbLeds = 6; // Two rgbLeds = 2*3 pins
-static const int g_numOfButtons = 4; 
+static const int g_numOfButtons = 4;
 
 // Global variables
 int currentIndex = 0; // colourpicker function variable
-
+int currentIndexSignal = 0;
+float currentRedLedBrightness = 0;
+float previousRedLedBrightness = 0;
 // Serial line for printf output
 Serial g_pc(USBTX, USBRX);
 
 // All redLEDs on K64F-KIT
 DigitalOut g_redLeds[g_numOfRedLeds] = {(PTA1),(PTA1),(PTC0),(PTC1),(PTC2),(PTC3),(PTC4),(PTC5),(PTC7),(PTC8)};
 
-// All rgbLEDs on K644-KIT 
+// All rgbLEDs on K644-KIT
 DigitalOut g_rgbLeds[g_numOfRgbLeds] = {(PTB2),(PTB3),(PTB9),(PTB11),(PTB18),(PTB19)};
 
 // All Buttons on K64F-KIT
@@ -32,7 +35,7 @@ class PwmLed{
 
 	public:
 		// Constructor
-		PwmLed(PinName pin, int brightness) 
+		PwmLed(PinName pin, int brightness)
 		{
 			this->led = new DigitalOut(pin); //Creation of new DigitalOut object (Led)
 			this->setBrightness(brightness); //Using setter to recalculate percents to float variable
@@ -40,17 +43,17 @@ class PwmLed{
 		}
 
 		// brightness setter, recalculates int value representing percents of brightness to float variable
-		void setBrightness(int brightness){
-			this->brightness = (float)brightness/100;
+		void setBrightness(float brightness){
+			this->brightness = brightness;
 		}
 
-		/* Function which is called timeUnit times. It determines if led is on or off in the current timeFrame 
-		This is basicaly how pwm is implemented. The program runs so quickly that you are not able to see if the led 
+		/* Function which is called timeUnit times. It determines if led is on or off in the current timeFrame
+		This is basicaly how pwm is implemented. The program runs so quickly that you are not able to see if the led
 		is on or off. Number of timeFrames in which led is on increases the brightness of led. */
 		void update()
 		{
 			// Reset of timeFrame counter after it reaches timeUnit
-			if(this->timeFrame == timeUnit){ 
+			if(this->timeFrame == timeUnit){
 				this->timeFrame = 0;
 			}
 			/* This calculation determines if led should be on or not, in dependency on brightness.
@@ -96,6 +99,8 @@ PwmLed pwmLeds[g_numOfPwmLeds] =
 
 };
 
+
+
 // Function which calls update on every led in pwmLeds array
 void updater(){
 	for (int l_currentLed = 0; l_currentLed < g_numOfPwmLeds; l_currentLed++){
@@ -114,6 +119,7 @@ void policie(){;
 	pwmLeds[10].setBrightness(1.0);
 	pwmLeds[11].setBrightness(1.0);
 	pwmLeds[13].setBrightness(0);
+	wait_ms(100);
 
 }
 
@@ -164,6 +170,46 @@ void colourPicker(){
 	}
 }
 
+void signal(){
+	// Go to next led
+	if(!g_buttons[0] && pwmLeds[8].getBrightness() < 1.0){
+		pwmLeds[8].setBrightness(pwmLeds[8].getBrightness()+ledIncrease);
+		wait_ms(100);
+	}
+	if(!g_buttons[1] && pwmLeds[9].getBrightness() < 1.0){
+		pwmLeds[9].setBrightness(pwmLeds[9].getBrightness()+ledIncrease);
+		wait_ms(100);
+	}
+	if(!g_buttons[2] && pwmLeds[10].getBrightness() < 1.0){
+		pwmLeds[10].setBrightness(pwmLeds[10].getBrightness()+ledIncrease);
+		wait_ms(100);
+	}
+	if(g_buttons[0] && pwmLeds[8].getBrightness() > 0.0){
+		pwmLeds[8].setBrightness(pwmLeds[8].getBrightness()-ledIncrease);wait_ms(100);
+	}
+	if(g_buttons[1] && pwmLeds[9].getBrightness() > 0.0){
+		pwmLeds[9].setBrightness(pwmLeds[9].getBrightness()-ledIncrease);wait_ms(100);
+	}
+	if(g_buttons[2] && pwmLeds[10].getBrightness() > 0.0){
+		pwmLeds[10].setBrightness(pwmLeds[10].getBrightness()-ledIncrease);wait_ms(100);
+	}
+	if(!g_buttons[3]){
+		for(int i = 0; i < g_numOfPwmLeds - 3; i++){
+			pwmLeds[i].setBrightness(0);
+		}
+	}
+
+	previousRedLedBrightness = currentRedLedBrightness;
+	currentRedLedBrightness = (pwmLeds[8].getBrightness()+pwmLeds[9].getBrightness()+pwmLeds[10].getBrightness())/3;
+	if(currentRedLedBrightness != previousRedLedBrightness){
+		g_pc.printf("Current redLEDs brightness: ");
+		g_pc.printf("%f\r\n", currentRedLedBrightness);
+	}
+	for(int i = 0; i < g_numOfPwmLeds - 6; i++){
+		pwmLeds[i].setBrightness(currentRedLedBrightness);
+	}
+}
+
 int main()
 {
 	// Serial line initialization
@@ -178,6 +224,8 @@ int main()
 	{
 		//policie();
 		//colourPicker();
+		//signal();
+
 	}
 }
 
