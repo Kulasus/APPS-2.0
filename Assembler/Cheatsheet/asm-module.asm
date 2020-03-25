f8b9b52921f034e3da1f1929f1c9c6aa6abd2924
lolray <60922832+lolray@users.noreply.github.com>
2020-03-25 14:52:52 +0100

    Add files via upload

--- Assembler/Cheatsheet/asm-module.asm
+++ Assembler/Cheatsheet/asm-module.asm
@@ -1,370 +1,86 @@
-; 	This defines in which mode will nasm compile the code
 	bits 64
-; 	Data section is used for declaring variables
+
 	section .data
 
-; Variables
-; 	extern means variables are not declared in this file
-;-----------------------------------------------------------------
-;   VARIABLES FOR PART 1
-;-----------------------------------------------------------------
-	extern g_char, g_num32, g_num64
-	extern g_array_char, g_array_num32, g_array_num64
-	extern g_index, g_index_plus_one
-	extern g_firstIndexOfArray, g_lastIndexOfArray
-	extern g_switchIndex1, g_switchIndex2
+ extern g_val0, g_val1
+ extern g_pole_long1, g_pole_long2, g_pozdrav
+ extern g_index1
+ extern g_long,g_lsb,g_msb
 
-;-----------------------------------------------------------------
-;   VARIABLES FOR PART 2
-;-----------------------------------------------------------------
-	extern g_rgb565_b1, g_rgb565_b2
-	extern g_char_array, g_char_mean
-	extern g_number
-	extern g_int_array, g_int_array_sum
-	extern g_positive, g_negative
-	extern g_string_len, g_string
-; Text section is used for defining functions
-	section .text
-;*****************************************************************
-;-----------------------------------------------------------------
-;   FUNCTIONS FOR PART 1
-;-----------------------------------------------------------------
-	; Definition of function set_variables_in_asm
-	global set_variables_in_asm
-set_variables_in_asm:
-	enter 0,0 ; Equiualent to {
+        section .text
+
+	global uloha1
+uloha1:
+	enter 0,0
+
 	
-	; Assigning variables value from memory to memory
-	mov byte [g_char], 'x' 
-	mov dword [g_num32], 32323232
-	mov qword [g_num64], 64646464
-
-	leave	; Equiualent to }
-	ret
-
-;*****************************************************************
-	; Definition of function move_variables_in_asm
-	global move_variables_in_asm
-move_variables_in_asm:
-	enter 0,0
-
-	; Extension of g_num32 by movsx because g_num32 is 32bit and RCX is 64bit
-	movsx RCX, dword[g_num32]
-	mov [g_num64], RCX
-
-	; The same, only difference here is we are extending g_char only to 32bit
-	movsx EAX, byte[g_char]
-	mov [g_num32], EAX
+	movsx RCX, dword [g_val0]
+	mov [g_pole_long1 + 0 * 8], RCX 
+	movsx RCX, dword [g_val0]
+	mov [g_pole_long1 + 2 * 8], RCX 
+	movsx RCX, dword [g_val0]
+	mov [g_pole_long1 + 4 * 8], RCX 
+	movsx RCX, dword [g_val1] 
+	mov [g_pole_long1 + 1 * 8], RCX 
+	movsx RCX, dword [g_val1] 
+	mov [g_pole_long1 + 3 * 8], RCX 
+	movsx RCX, dword [g_val1] 
+	mov [g_pole_long1 + 5 * 8], RCX 
 
 	leave
 	ret
 
-;*****************************************************************
-	; Definition of function set_array_char_in_asm
-	global set_array_char_in_asm
-set_array_char_in_asm:
+	global uloha2
+uloha2:
 	enter 0,0
 
-	mov byte [g_array_char], 'K'
-	mov byte [g_array_char + 1], 'O'
-	mov byte [g_array_char + 2], 'K'
-	mov byte [g_array_char + 3], 'O'
+	mov RCX, qword [g_val0]
+	mov [g_pole_long2 + 0 * 8], RCX 
+	mov RCX, qword [g_val0]
+	mov [g_pole_long2 + 2 * 8], RCX 
+	mov RCX, qword [g_val0]
+	mov [g_pole_long2 + 4 * 8], RCX 
+	mov RCX, qword [g_val1] 
+	mov [g_pole_long2 + 1 * 8], RCX 
+	mov RCX, qword [g_val1] 
+	mov [g_pole_long2 + 3 * 8], RCX 
+	mov RCX, qword [g_val1] 
+	mov [g_pole_long2 + 5 * 8], RCX 
 
 	leave
 	ret
 
-;*****************************************************************
-	; Definition of function set_array_num64_in_asm
-	global set_array_num64_in_asm
-set_array_num64_in_asm:
+	global uloha3
+uloha3:
 	enter 0,0
 
-	; 1 * 8 because + 1 increases adress only by 8bits
-	mov qword [g_array_num64 + 0 * 8], 1000
-	mov qword [g_array_num64 + 4 * 8], 1000
-	mov qword [g_array_num64 + 9 * 8], 1000
+	mov BL, [g_pozdrav +1]
+	mov [g_pozdrav + 0], BL
+	mov BL, [g_pozdrav + 2]
+	mov [g_pozdrav + 1], BL
+	mov BL, [g_pozdrav + 3]
+	mov [g_pozdrav + 2], BL
+	mov BL, [g_pozdrav + 4]
+	mov [g_pozdrav + 3], BL
+	mov BL, [g_pozdrav + 5]
+	mov [g_pozdrav + 4], BL
+	mov BL, [g_pozdrav + 6]
+	mov [g_pozdrav + 5], BL
 
 	leave
 	ret
 
-;*****************************************************************
-	; Definition of function set_array_num32_index_in_asm
-	global set_array_num32_index_in_asm
-set_array_num32_index_in_asm:
+	global uloha5
+uloha5:
 	enter 0,0
 
-	movsx RAX, dword [g_index]
-	mov dword [g_array_num32 + RAX * 4 ], 3333
+	mov EAX, [g_long]
+    mov [g_lsb], EAX
+	mov EAX, [g_long + 4]
+	mov [g_msb], EAX
 
 	leave
 	ret
 
-;*****************************************************************
-	; Definition of function set_array_num32_index_num32_in_asm
-	global set_array_num32_index_num32_in_asm
-set_array_num32_index_num32_in_asm:
-	enter 0,0
 
-	mov EAX, [g_index]
-	mov EBX, [g_num32]
-	mov [g_array_num32 + EAX * 4], EBX
-
-	leave
-	ret
-
-;*****************************************************************
-	; Definition of function switch_between_array_num32_array_num64_in_asm
-	global switch_between_array_num32_array_num64_in_asm
-switch_between_array_num32_array_num64_in_asm:
-	enter 0,0
-
-	mov EAX, [g_array_num32 + 0 * 4]
-	mov RBX, [g_array_num64 + 0 * 8]
-	mov [g_array_num64 + 0 * 8], RAX
-	mov [g_array_num32 + 0 * 4], EBX
-	mov ECX, [g_array_num32 + 1 * 4]
-	mov RDX, [g_array_num64 + 1 * 8]
-	mov [g_array_num64 + 1 * 8], RCX
-	mov [g_array_num32 + 1 * 4], EDX
-	mov EDI, [g_array_num32 + 2 * 4]
-	mov RSI, [g_array_num64 + 2 * 8]
-	mov [g_array_num64 + 2 * 8], RDI
-	mov [g_array_num32 + 2 * 4], ESI
-
-	leave
-	ret
-
-;*****************************************************************
-	; Definition of function switch_first_and_last_index_array_num32_in_asm
-	global switch_first_and_last_index_array_num32_in_asm
-switch_first_and_last_index_array_num32_in_asm:
-	enter 0,0
-
-	mov ECX, [g_firstIndexOfArray]
-	mov EDX, [g_lastIndexOfArray]
-	mov EDI, [g_array_num32 + ECX * 4]
-	mov ESI, [g_array_num32 + EDX * 4]
-	mov [g_array_num32 + EDX * 4], EDI
-	mov [g_array_num32 + ECX * 4], ESI
-
-	leave
-	ret
-
-;*****************************************************************
-	; Definition of function switch_neighbors_array_num32_in_asm
-	global switch_neighbors_array_num32_in_asm
-switch_neighbors_array_num32_in_asm:
-	enter 0,0
-
-	mov EAX, [g_array_num32 + 0 * 4]
-	mov EBX, [g_array_num32 + 1 * 4]
-	mov [g_array_num32 + 0 * 4], EBX
-	mov [g_array_num32 + 1 * 4], EAX
-	mov EAX, [g_array_num32 + 2 * 4]
-	mov EBX, [g_array_num32 + 3 * 4]
-	mov [g_array_num32 + 2 * 4], EBX
-	mov [g_array_num32 + 3 * 4], EAX
-	mov EAX, [g_array_num32 + 4 * 4]
-	mov EBX, [g_array_num32 + 5 * 4]
-	mov [g_array_num32 + 4 * 4], EBX
-	mov [g_array_num32 + 5 * 4], EAX
-	mov EAX, [g_array_num32 + 6 * 4]
-	mov EBX, [g_array_num32 + 7 * 4]
-	mov [g_array_num32 + 6 * 4], EBX
-	mov [g_array_num32 + 7 * 4], EAX
-	mov EAX, [g_array_num32 + 8 * 4]
-	mov EBX, [g_array_num32 + 9 * 4]
-	mov [g_array_num32 + 8 * 4], EBX
-	mov [g_array_num32 + 9 * 4], EAX
-
-	leave
-	ret
-
-;*****************************************************************
-	; Definition of function set_array_num32_neighbors_value
-	global set_array_num32_neighbors_value
-set_array_num32_neighbors_value:
-	enter 0,0
-
-	mov EAX, [g_index]
-	mov ECX, [g_index_plus_one]
-	mov EBX, [g_array_num32 + ECX * 4]
-	mov [g_array_num32 + EAX * 4], EBX
-
-	leave
-	ret
-
-;******************************************************************
-	; Definition of function switch_two_values_index_num32_in_asm	
-	global switch_two_values_index_num32_in_asm
-switch_two_values_index_num32_in_asm:
-	enter 0,0
-
-	mov EAX, [g_switchIndex1]
-	mov EBX, [g_switchIndex2]
-	mov ECX, [g_array_num32 + EAX * 4]
-	mov EDX, [g_array_num32 + EBX * 4]
-	mov [g_array_num32 + EAX * 4], EDX
-	mov [g_array_num32 + EBX * 4], ECX
-
-	leave
-	ret
-
-;-----------------------------------------------------------------
-;   FUNCTIONS FOR PART 2
-;-----------------------------------------------------------------
-;******************************************************************
-	; Definition of function move_blue
-	global move_blue
-move_blue:
-	enter 0,0
-
-	mov AX, 0b11111         ; blue mask -> 0b11111 = 0b0000000000011111
-	mov CX, [g_rgb565_b1] 
-	and CX, AX				; only blue
-	not AX					; mask for green and red
-	and [g_rgb565_b2], AX	; only red and green
-	or [g_rgb565_b2], CX
-
-	leave
-	ret
-;******************************************************************
-	; Definition of function mean_char_array
-	global mean_char_array
-mean_char_array:
-	enter 0,0
-
-	movsx RAX, byte [g_char_array]  ; sum
-	movsx RCX, byte [g_char_array + 1]
-	add RAX, RCX 					; sum += g_char_array[1]
-	movsx RCX, byte [g_char_array + 2]
-	add RAX, RCX 					; sum += g_char_array[2]
-	movsx RCX, byte [g_char_array + 3]
-	add RAX, RCX 					; sum += g_char_array[3]
-
-	shr RAX, 2						; sum / 4
-	mov [g_char_mean], RAX
-
-	leave
-	ret
-;******************************************************************
-	; Definition of function number_mul100
-	global number_mul100
-number_mul100:
-	enter 0,0
-
-	mov ECX, [g_number]
-	shl ECX, 2				; *4
-	mov EAX, ECX
-	shl EAX, 3				; *4*8
-	add ECX, EAX
-	shl EAX, 1				; *4*8*2
-	add ECX, EAX
-
-	mov [g_number], ECX 
-
-	leave
-	ret
-
-;******************************************************************
-	; Definition of function sum_int_array
-	global sum_int_array
-sum_int_array:
-	enter 0,0
-	
-	xor EAX, EAX   		; mov EAX, 0 / sub EAX, EAX
-	; for (rdx = 0; rdx < 10; rdx++){}
-	mov RDX, 0
-.back:
-	cmp RDX, 10 		; sub rdx, 10
-	jge .endfor
-
-	add EAX, [g_int_array + RDX * 4]
-
-	inc RDX
-	jmp .back
-	; endfor
-.endfor:
-	mov [g_int_array_sum], EAX
-	leave
-	ret
-
-
-;******************************************************************
-	; Definition of function posneg_int_array
-	global posneg_int_array
-posneg_int_array:
-	enter 0,0
-
-	mov RDX, 0
-.back:
-	cmp RDX, 10 		
-	jge .endfor
-
-	cmp dword [g_int_array + RDX * 4], 0
-	je .continue
-	jg .positive
-	inc dword [g_negative]
-	jmp .continue
-
-.positive:
-	inc dword [g_positive]
-	jmp .continue
-.continue:
-	inc RDX
-	jmp .back
-.endfor:
-	leave
-	ret
-
-
-;******************************************************************
-	; Definition of function string_len
-	global string_len
-string_len:
-	enter 0,0
-
-	mov RCX, g_string ; -> no []? it means pointers bruh ;)
-
-.back:
-	cmp byte [RCX], 0
-	je .end
-	inc RCX
-	jmp .back
-
-.end:
-	sub RCX, g_string
-	mov [g_string_len], ECX
-
-	leave
-	ret
-
-;******************************************************************
-	; Definition of function string_low
-	global string_low
-string_low:
-	enter 0,0
-
-	mov RCX, 0
-
-.back:
-	cmp byte [g_string + RCX], 0
-	je .end
-	cmp byte [g_string + RCX], 'A'
-	jb .continue
-	cmp byte [g_string + RCX], 'Z'
-	ja .continue
-	add byte [g_string + RCX], 'a' - 'A'
-
-
-.continue:
-	inc RCX
-	jmp .back
-
-.end:
-	sub RCX, g_string
-	mov [g_string_len], ECX
-
-
-	leave
-	ret
+; labels:
--- Assembler/Cheatsheet/c-main.c
+++ Assembler/Cheatsheet/c-main.c
@@ -1,202 +1,46 @@
-
 #include <stdio.h>
 
-//--------PART-1--MOV/MOVSX/MOVZX----------------------------------------------------------------------------------------------------------
-
-#define numArrayLength 10
+#define delkaPoleLong 6
+#define delkaPoleChar 32
 
 // Variables
-char g_char = 0;
-int g_num32 = 0;
-long g_num64 = 0;
-int g_index = 2;
-int g_index_plus_one = 3;
-int g_firstIndexOfArray = 0;
-int g_lastIndexOfArray = numArrayLength - 1;
-int g_switchIndex1 = 0;
-int g_switchIndex2 = 2;
+int g_val0 = -5, g_val1 = -25; 
+long g_pole_long1[ delkaPoleLong ] = { 0, 0, 0, 0, 0, 0 };
+long g_pole_long2[ delkaPoleLong ] = { 0, 0, 0, 0, 0, 0 };
+char g_pozdrav[ delkaPoleChar ] = "_Ahoj!";
+long g_long = 0xFEDCBA0987654321;
+int g_lsb = 0, g_msb = 0;
 
-char g_array_char[128] = "Some message";
-int g_array_num32[numArrayLength] = {0,1,2,3,4,5,6,7,8,9};
-long g_array_num64[numArrayLength] = {10,11,12,13,14,15,16,17,18,19};
+void uloha1();
+void uloha2();
+void uloha3();
+void uloha5();
 
-// Functions which are defined in assembler
-void set_variables_in_asm(); // Sets variables values in assembler
-void move_variables_in_asm(); // Moves g_char -> g_num32 and g_num32 -> g_num64 in assembler 
-void set_array_char_in_asm(); // Sets first 4 characters of g_array_char in assembler
-void set_array_num32_index_in_asm(); // Sets certain values on index declared in c in g_array_num32 in assembler
-void set_array_num64_in_asm(); // Sets certain values on certain indexes in g_array_num64 in assembler
-void set_array_num32_index_num32_in_asm(); // Sets values declared in c into index declared in c in g_array_num32 in assembler
-void switch_two_values_index_num32_in_asm(); // Switches two values on certain indexes in g_array_num32 in assembler
-void switch_first_and_last_index_array_num32_in_asm(); // Switches values on first and last index of g_array_num32 in assembler
-void switch_between_array_num32_array_num64_in_asm(); // Switches first three values from g_array_num32 to g_array_num64 and backwards in assembler
-void switch_neighbors_array_num32_in_asm(); // Switches neighbors values in g_array_num32 -> WORKS ONLY FOR array lenght = 10
-void set_array_num32_neighbors_value(); // Sets value of entity on certain index to value of its right neigbour
-
-void print_array_num64(){ // Prints values in g_array_num64
-    for (int i = 0; i < numArrayLength; i++)
+void vypis_pole_long(long *g_pole_long){
+    for (int i = 0; i < 6; i++)
     {
-        printf("%ld", g_array_num64[i]);
+        printf("%ld", g_pole_long[i]);
         printf(",");
     }
     printf("\n");
 }
 
-void print_array_num32(){ // Prints values in g_array_num32
-    for (int i = 0; i < numArrayLength; i++)
-    {
-        printf("%d", g_array_num32[i]);
-        printf(",");
-    }
-    printf("\n");
-}
-
-void switch_neigbors_refactored_array_num32_in_asm(){ // Switches neighbors values in g_array_num32, works for variable array length
-    for (int i = 0; i < numArrayLength; i+=2)
-    {
-        g_switchIndex1 = i;
-        g_switchIndex2 = i + 1;
-        if(g_switchIndex2 >= numArrayLength){
-            break;
-        }
-        switch_two_values_index_num32_in_asm();
-    }
-}
-
-//--------PART-2--ARYTHMETICAL OPERATIONS----------------------------------------------------------------------------------------------------------
-
-// Variables
-unsigned short g_rgb565_b1 = 0b1100011100011000;
-unsigned short g_rgb565_b2 = 0b0000011100001110;
-char g_char_array[4] = {-128,-128,-128,-128};
-char g_char_mean = 0;
-int g_number = 3333;
-int g_int_array[10] = {10,20,-30,0,50,60,70,80,90,100};
-int g_int_array_sum = 0;
-int g_positive = 0;
-int g_negative = 0;
-char g_string[] = "Jebem ti boga!";
-int g_string_len = 0;
-
-// Functions which are defined in assembler
-void move_blue();
-void mean_char_array();
-void number_mul100(); // x*100 = x*(4+96) = x*(4+32+64)
-void sum_int_array();
-void posneg_int_array();
-void string_len();
-void string_low();
-//--------MAIN-------------------------------------------------------------------------------------------------------------------------------------
 int main()
 {
-    // Uncomment whatever you want -> One comment block = one function
-
-    /*
-    printf("g_char %c g_num32 %d g_num64 %ld\n",g_char,g_num32,g_num64);
-    set_variables_in_asm();
-    printf("g_char %c g_num32 %d g_num64 %ld\n",g_char,g_num32,g_num64);
-    move_variables_in_asm();
-    printf("g_char %c g_num32 %d g_num64 %ld\n",g_char,g_num32,g_num64);
-    */
-
-    /*
-    printf("g_array_char '%s'\n",g_array_char);
-    set_array_char_in_asm();
-    printf("g_array_char '%s'\n",g_array_char);
-    */
-
-    /*
-    print_array_num64();
-    set_array_num64_in_asm();
-    print_array_num64();
-    */
-
-    /*
-    print_array_num32();
-    set_array_num32_index_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    g_num32 = 88888;
-    g_index = 8;
-    print_array_num32();
-    set_array_num32_index_num32_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    print_array_num32();
-    switch_first_and_last_index_array_num32_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    print_array_num32();
-    print_array_num64();
-    switch_between_array_num32_array_num64_in_asm();
-    print_array_num32();
-    print_array_num64();
-    */
-
-    /*
-    print_array_num32();
-    switch_neighbors_array_num32_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    g_index_plus_one = g_index +1;
-    print_array_num32();
-    set_array_num32_neighbors_value();
-    print_array_num32();
-    */
-
-    /*
-    print_array_num32();
-    switch_two_values_index_num32_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    print_array_num32();
-    switch_neigbors_refactored_array_num32_in_asm();
-    print_array_num32();
-    */
-
-    /*
-    printf("b1 %04X b2 %04X\n", g_rgb565_b1,g_rgb565_b2);
-    move_blue();
-    printf("b1 %04X b2 %04X\n", g_rgb565_b1,g_rgb565_b2);
-    */
-   
-    /*
-    mean_char_array();
-    printf("mean: %d\n", g_char_mean);
-    */
-
-    /*
-    number_mul100();
-    printf("number %d\n", g_number);
-    */
-
-    /*
-    sum_int_array();
-    printf("suma: %d\n", g_int_array_sum);
-    */
-
-    /*
-    posneg_int_array();
-    printf("pos: %d neg: %d\n", g_positive, g_negative);
-    */
-
-    /*
-    string_len();
-    printf("delka je %d", g_string_len);
-    */
-
-    /*
-    string_low();
-    printf("string %s\n", g_string);
-    */
+   uloha1();
+   printf("Uloha1 :");
+   vypis_pole_long(g_pole_long1);
+   printf("\n");
+   uloha2();
+   printf("Uloha2 :");
+   vypis_pole_long(g_pole_long2);
+   printf("\n");
+   printf("Uloha3: '%s'\n",g_pozdrav);
+    uloha3();
+   printf("Uloha3: '%s'\n",g_pozdrav);
+   uloha5();
+   printf("\n");
+   printf("Uloha5 :");
+   printf( "long %016lX, msb %08X, lsb %08X\n", g_long, g_msb, g_lsb );
+   printf("\n");
 }
